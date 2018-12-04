8d224bbabe9cfbacc85c826c4d53e7ac849149e0
hao <winding345@gmail.com>
2018-12-04 16:15:27 +0800

    ~

--- db/write_batch.cc
+++ db/write_batch.cc
@@ -399,7 +399,7 @@ Status ReadRecordFromWriteBatch(Slice* input, char* tag,
 Status WriteBatch::Iterate(Handler* handler) const {
   Slice input(rep_);
   std::cout<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~add by hao~~~~~~~~~~~~~~~"<<std::endl;
-  std::cout<<rep_<<endl;
+  std::cout<<rep_<<std::endl;
   if (input.size() < WriteBatchInternal::kHeader) {
     return Status::Corruption("malformed WriteBatch (too small)");
   }
