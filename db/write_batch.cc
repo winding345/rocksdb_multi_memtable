c3ccf947db24cfb3b955f3430ca1d56f3296c1df
hao <winding345@gmail.com>
2018-12-13 16:57:28 +0800

    ~

--- db/write_batch.cc
+++ db/write_batch.cc
@@ -1241,14 +1241,14 @@ class MemTableInserter : public WriteBatch::Handler {
     MemTable* mem = cf_mems_->GetMemTable();
 
     int memtable_index = atoi(std::string(key.data(),key.size() < 2 ? key.size():2).c_str());
-    int key_num = atoi(std::string(key.data(),8).c_str());
-    printf("key_num %d\n",key_num);
-    printf("memtable_index %d\n",memtable_index);
-//    for(size_t x = 0;x < key.size();++x)
-//    {
-//        printf("%x\t",*(key.data()+x));
-//    }
-//    printf("\n");
+//    int key_num = atoi(std::string(key.data(),8).c_str());
+//    printf("key_num %d\n",key_num);
+//    printf("memtable_index %d\n",memtable_index);
+    for(size_t x = 0;x < 8;++x)
+    {
+        printf("%x\t",*(key.data()+x));
+    }
+    printf("\n");
 
 
     MemTable* mymem = cf_mems_->GetMymemTable(memtable_index);
