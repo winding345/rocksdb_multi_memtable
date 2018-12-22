3d9c5609bf9bb7b9a301f5a64aa362a9f6c76ba2
hao <winding345@gmail.com>
2018-12-22 10:06:39 +0800

    ~

--- db/column_family.cc
+++ db/column_family.cc
@@ -359,8 +359,8 @@ bool SuperVersion::Unref() {
 void SuperVersion::Cleanup() {
   assert(refs.load(std::memory_order_relaxed) == 0);
   imm->Unref(&to_delete);
-  mem->Unref();
-  MemTable* m = mymem[mymem_index]->Unref();
+  MemTable* m = mem->Unref();
+  mymem[mymem_index]->Unref();
   if (m != nullptr) {
     auto* memory_usage = current->cfd()->imm()->current_memory_usage();
     assert(*memory_usage >= m->ApproximateMemoryUsage());
