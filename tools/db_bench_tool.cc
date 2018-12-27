e79d9dd49982bf05b3bbfb2db0832d1063a894e2
hao <winding345@gmail.com>
2018-12-27 14:11:14 +0800

    ~

--- tools/db_bench_tool.cc
+++ tools/db_bench_tool.cc
@@ -7,6 +7,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file. See the AUTHORS file for names of contributors.
 
+#define PRINT_EVERY_GB_AVG_SPEED
+#define PRINT_EVERY_10S_SPEED
+
 #ifndef __STDC_FORMAT_MACROS
 #define __STDC_FORMAT_MACROS
 #endif
